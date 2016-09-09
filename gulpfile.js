a65d7a086d7b12e19c6cc474fa0889a572fc9b51
Karol P <karol.wsnyc@gmail.com>
2016-09-09 09:17:11 +0200

    Add gulp serve task.

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,69 @@
 // Modules
 // ======================================================
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 var $ = require('gulp-load-plugins')();
 
 //
 // Styles (SCSS + autoprefixer + minify)
 // ======================================================
 gulp.task('styles', function () {
-    return gulp.src('./src/scss/**/*.scss')
-        .pipe($.sass())
-        .pipe($.autoprefixer({browsers: ['last 10 version']}))
-        .pipe($.csso())
-        .pipe(gulp.dest('./dist/css/'));
+   return gulp.src('./scss/**/*.scss')
+       .pipe($.sass())
+       .pipe($.autoprefixer({browsers: ['last 10 version']}))
+       .pipe($.csso())
+       .pipe(gulp.dest('./css/'))
+       .pipe(browserSync.stream());
 });
 
 
 // Watch
+// ======================================================
 gulp.task('watch', function () {
 
-    // Watch .scss files
-    gulp.watch('/src/scss/**/*.scss', ['styles']);
+// Watch .scss files
+   gulp.watch('./scss/**/*.scss', ['styles']);
 
-    // // Watch .js files
-    // gulp.watch('/src/js/*.js', ['js']);
-    //
-    // // Watch image files
-    // gulp.watch('/src/images/**/*', ['image']);
+   // // Watch .js files
+   gulp.watch('./js/*.js', ['js']);
+   //
+   // // Watch image files
+   gulp.watch('./img/**/*', ['image']);
+
+   // add browserSync.reload to the tasks array to make
+   // all browsers reload after tasks are complete.
+   gulp.watch('./*.html').on('change', browserSync.reload);
 });
+
+
+// Static server
+// ======================================================
+gulp.task('serve', function() {
+   browserSync.init({
+       server: {
+           baseDir: "./",
+           notify: true
+       }
+   });
+
+   // Watch .scss files
+   gulp.watch('./scss/**/*.scss', ['styles']);
+
+   // // Watch .js files
+   //gulp.watch('./js/*.js', ['js']);
+   //
+   // // Watch image files
+   //gulp.watch('./img/**/*', ['image']);
+
+   // add browserSync.reload to the tasks array to make
+   // all browsers reload after tasks are complete.
+   gulp.watch('./*.html').on('change', browserSync.reload);
+});
+
+
+
+
+// Initialization - default tasks 
+// $ gulp
+// ======================================================
+gulp.task('default', ['styles', 'serve']);
